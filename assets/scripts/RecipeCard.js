5391a0c5186dc54be0b6d7d11820ed1b22da9a51
TristonBabers <Triston.babers.official@gmail.com>
2022-11-07 15:48:26 -0800

    Finished Explore

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -15,10 +15,10 @@ class RecipeCard extends HTMLElement {
 
 
     // A2. TODO - Create an <article> element - This will hold our markup once our data is set
-    let elementArticle = document.createElement('article');
+    const elementArticle = document.createElement('article');
 
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
-    let elementStyle = document.createElement('style');
+    const elementStyle = document.createElement('style');
 
     // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
     elementStyle.textContent = 
@@ -128,7 +128,7 @@ class RecipeCard extends HTMLElement {
     if (!data) return;
 
     // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
-    let selectArticle = this.shadowRoot.firstChild;
+    const selectArticle = this.shadowRoot.firstChild;
     
     //console.log(document.querySelector('article'));
     // A7. TODO - Set the contents of the <article> with the <article> template given in
